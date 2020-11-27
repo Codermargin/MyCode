782f02c614151ed0525262222f91b03f9bec29b5
EvolutionMar <1273930793@qq.com>
2020-11-27 22:42:04 +0800

    做题-四数相加_454

---
+++ ITLearn/src/main/java/com/xyx/DailyLeetCode/fourSumCount_454.java
@@ -0,0 +1,36 @@
+package com.xyx.DailyLeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fourSumCount_454 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 四种和计算
+     * @param A
+     * @param B
+     * @param C
+     * @param D
+     * @return
+     */
+    public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : A) {
+            for (int v : B) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : C) {
+            for (int v : D) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
